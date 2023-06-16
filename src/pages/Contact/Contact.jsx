14370b50f474cfcf65cd66fe5664334cadd51982
Leonardo Becerril <leonardo.bazan@ssedomex.gob.mx>
2023-06-15 18:00:52 -0600

    fix: console errors

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,8 @@
 import {Helmet} from "react-helmet-async";
 import {Image} from 'mui-image';
 import ContactImage from '../../assets/images/Contact.webp'
-import {Button, Grid, Typography} from "@mui/material";
+import {Grid} from "@mui/material";
+
 const Contact = () => {
 
 
@@ -10,38 +11,30 @@ const Contact = () => {
             <Helmet>
                 <title>Pablo Marte - Contact</title>
             </Helmet>
-           <Grid container alignItems={'center'} justifyContent={'center'} textAlign={'center'}>
-               <Grid item>
-                   <Image
-                       src={ContactImage}
-                       height="750px"
-                       width="500px"
-                       duration={
-                           3000
-                       }
-                       easing="cubic-bezier(0.7, 0, 0.6, 1)"
-                       showLoading={false}
-                       errorIcon={true}
-                       shift="null"
-                       distance="100px"
-                       shiftDuration={
-                           900
-                       }
-                       bgColor="inherit"
-                   />
-               </Grid>
-               <Grid item xs={12} sx={{my: 4}}>
-                   <Typography variant={'h4'} component={'h2'}>
-                       Ultimo Lanzamiento
-                   </Typography>
-                   <Button variant={'contained'} component={'a'} size="large" sx={{borderRadius: 20, my: 2}}
-                           href={'https://onerpm.link/326059895093'} target="_blank"
-                           rel="noopener noreferrer"
-                   >
-                       ESCÚCHALO AQUÍ
-                   </Button>
-               </Grid>
-           </Grid>
+            <Grid container alignItems={'center'} justifyContent={'center'} textAlign={'center'}>
+                <Grid item xs={'auto'}>
+                    <Image
+                        src={ContactImage}
+                        height="750px"
+                        width="500px"
+                        duration={
+                            3000
+                        }
+                        easing="cubic-bezier(0.7, 0, 0.6, 1)"
+                        showLoading={false}
+                        errorIcon={true}
+                        shift="null"
+                        distance="100px"
+                        shiftDuration={
+                            900
+                        }
+                        bgColor="inherit"
+                    />
+                </Grid>
+                <Grid item xs={12} sx={{my: 4}}>
+
+                </Grid>
+            </Grid>
         </>
     );
 };
--- src/pages/Music/Nightingale.jsx
+++ src/pages/Music/Nightingale.jsx
@@ -13,7 +13,7 @@ const Nightingale = () => {
             <title>Pablo Marte - Nightingale</title>
         </Helmet>
         <Grid container alignItems={'center'} justifyContent={'center'} textAlign={'center'}>
-            <Grid item>
+            <Grid item xs="auto" sx={{my: 5}}>
                 <Typography variant={'h3'} component={'h1'}>
                     NIGHTINGALE
                 </Typography>
@@ -27,12 +27,33 @@ const Nightingale = () => {
                         sx={{cursor: 'pointer'}}
                     />
                 </Box>
-                <Button variant={'contained'} component={'a'} size="large" sx={{borderRadius: 20}}
+                <Button variant={'contained'} component={'a'} size="large" sx={{borderRadius: 20, my: 5}}
                         href={'https://distrokid.com/hyperfollow/pablomarte/nightingale'} target="_blank"
                         rel="noopener noreferrer"
                 >
                     ESCÚCHALO AQUÍ
                 </Button>
+                <hr/>
+            </Grid>
+            <Grid item xs={12} sx={{my: 5}}>
+                <Grid container alignItems={'center'} justifyContent={'center'} spacing={4}>
+                    <Grid item xs={10} md={3}>
+                        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameBorder="0"
+                                scrolling="no"
+                                height="450"   title={'nightingaleA'}
+                                style={{width: '100%', maxWidth: '660px', overflow: 'hidden', borderRadius: '10px'}}
+                                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+                                src="https://embed.music.apple.com/mx/album/nightingale-single/1688354063"></iframe>
+                    </Grid>
+                    <Grid item xs={10} md={3}>
+                        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameBorder="0"
+                                scrolling="no"
+                                height="450"   title={'nightingaleA'}
+                                style={{width: '100%', maxWidth: '660px', overflow: 'hidden', borderRadius: '10px'}}
+                                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+                                src="https://open.spotify.com/embed/album/4eUz0BNRBtImRrc7IJ72y7?utm_source=generator"></iframe>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     </>);
